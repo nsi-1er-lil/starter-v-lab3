a7c07a8699fd270f46879e085a19e5e6cfd99fb1
v-lab3 <vaniades@lyceechurchill.london>
2024-09-24 14:37:08 +0000

    test exo7

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -3,3 +3,15 @@ p=4*c
 a=c**2
 b=(a>5)
 print(b)
+def perimetre(x):
+    
+    p = 4 * x  
+    return p
+
+
+c = 2
+p = perimetre(c)
+a = c**2  
+b = (a > 5)  
+print(f"Périmètre: {p}")
+print(f"Surface supérieure à 5: {b}")
